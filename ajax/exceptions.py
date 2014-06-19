57f150c44061e7c0e85f59c4a7565fb98359d3bc
Kevin Klaes <kevin.klaes@gmail.com>
2014-06-18 20:49:22 -0500

    A default exception is passed back.
    The exception will always pass back a message even if the raise does not include one.

--- ajax/exceptions.py
+++ ajax/exceptions.py
@@ -8,6 +8,14 @@ from django.http import HttpResponse, HttpResponseNotFound, \
     HttpResponseForbidden, HttpResponseNotAllowed, HttpResponseServerError, \
     HttpResponseBadRequest
 
+try:
+    from settings import AJAX_DEFAULT_ERROR_MESSAGE
+except ImportError:
+    import warnings
+    warnings.warn("You should specifcy AJAX_DEFAULT_ERROR_MESSAGE in your \
+                   Django settings file.", ImportWarning)
+    AJAX_DEFAULT_ERROR_MESSAGE = 'There was an ajax error.'
+
 
 class AlreadyRegistered(Exception):
     pass
@@ -30,7 +38,7 @@ class AJAXError(Exception):
         500: HttpResponseServerError,
     }
 
-    def __init__(self, code, msg, **kwargs):
+    def __init__(self, code, msg=AJAX_DEFAULT_ERROR_MESSAGE, **kwargs):
         self.code = code
         self.msg = msg
         self.extra = kwargs  # Any kwargs will be appended to the output.
